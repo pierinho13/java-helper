207e1463486b0c73bb2f1f515b03357ecc949e43
Piero Rospigliosi <pierinho13@gmail.com>
2026-03-20 15:38:36 +0100

    default menu

--- cmd/root.go
+++ cmd/root.go
@@ -10,6 +10,9 @@ var rootCmd = &cobra.Command{
 	Use:   "java-helper",
 	Short: "Small CLI to run common Maven tasks for local Java projects",
 	Long:  "java-helper provides shortcuts for common Maven commands used in Java projects.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return menuCmd.RunE(cmd, args)
+	},
 }
 
 func Execute() {
